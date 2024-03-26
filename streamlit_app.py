470417a39a1f149988819838867e98b23f900d3b
ellen-barnes <70383813+ellen-barnes@users.noreply.github.com>
2024-03-26 14:14:07 +0000

    Update streamlit_app.py
    In for loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,9 @@ if ingredients_list:
     ingredients_string = ''
     for each_fruit in ingredients_list:
         ingredients_string +=  each_fruit + ' '
-    #st.write(ingredients_string)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
+
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string +"""','"""+name_on_order +"""')"""
@@ -44,7 +46,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon = '✅')
     
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
-fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
+
 
